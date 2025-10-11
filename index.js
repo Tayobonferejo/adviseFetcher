9f1d7645d9e66f5278b3f833a173a92d52d969cc
tayobonferejo <oluwatayogeorgeige@gmail.com>
2025-10-11 12:45:53 +0100

    api fetch

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 
-    async function fetchDataFromAPI() {
+    window.addEventListener("load",   async function fetchDataFromAPI() {
         try {
             const response = await fetch("https://api.adviceslip.com/advice");
             if(!response.ok) {
                 throw new Error(`HTTP error! Status: ${response.status}`)
             }
             const data = await response.json();
-            console.log(data)
+            console.log(data);
+            console.log(data.slip.advice);
             // return data;
+
+            const adviceContainer = document.getElementById("advice-container");
+            adviceContainer.innerHTML = data.slip.advice
         }
         catch (error) {
             console.error('Error fetching data:', error);
             throw error;
         }
-    }
-
-    fetchDataFromAPI();
+    })
